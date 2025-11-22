d25ce30773878dd6f985cf77dce65d6a784bd0ae
Sooraj1075 <soorajbro1111@gmail.com>
2025-11-22 13:16:04 +0530

    Create 104.java

---
+++ 104.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int maxDepth(TreeNode root) {
+        return height(root);
+        
+    }
+
+    int height(TreeNode node)
+    {
+        if(node == null)
+        {
+            return 0;
+        }
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+    
+        return Math.max(leftHeight,rightHeight) + 1;
+    }
+}
