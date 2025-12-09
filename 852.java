fd2757d93c1132abaa4437fc0bf0f8dcf7cf9b8f
Sooraj1075 <soorajbro1111@gmail.com>
2025-12-09 18:08:15 +0530

    Update 852.java

--- 852.java
+++ 852.java
@@ -3,7 +3,7 @@ class Solution {
         int s = 0;
         int e = arr.length - 1;
 
-        while(s <e)
+        while(s <=e)
         {
             int mid = s + (e-s)/2;
             if(arr[mid] < arr[mid+1])
