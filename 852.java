85cebc24bba970704304d9f5a6cb8abf114fdc7f
Sooraj1075 <soorajbro1111@gmail.com>
2025-12-09 18:02:50 +0530

    Create 852.java

---
+++ 852.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int s = 0;
+        int e = arr.length - 1;
+
+        while(s <=e)
+        {
+            int mid = s + (e-s)/2;
+            if(arr[mid] < arr[mid+1])
+            {
+                s = mid+1;
+            }
+            else
+            {
+                e = mid - 1;
+            }
+        }
+
+        return s;// use binary search for mid element first,elemnts first increase and then decreses
+    }
+}
